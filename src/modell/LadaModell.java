2d0e5330f40e05660bcbba2ce78f449bed865621
Bori888 <ilyes.bori8@gmail.com>
2025-10-02 15:38:25 +0200

    Modell ujra kesz

--- src/modell/LadaModell.java
+++ src/modell/LadaModell.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modell;
 
-/**
- *
- * @author ilyes.borbala
- */
 public class LadaModell {
+
+    private String Nev;
+    private String Felirat;
+    private boolean benneVanEAKincs;
+    private boolean IgazatMonddE;
+
+    public LadaModell(String Nev, String Felirat, boolean benneVanEAKincs, boolean IgazatMonddE) {
+        this.Nev = Nev;
+        this.Felirat = Felirat;
+        this.benneVanEAKincs = benneVanEAKincs;
+        this.IgazatMonddE = IgazatMonddE;
+    }
+
+    public String getNev() {
+        return Nev;
+    }
+
+    public String getFelirat() {
+        return Felirat;
+    }
+
+    public boolean isBenneVanEAKincs() {
+        return benneVanEAKincs;
+    }
+
+    public boolean isIgazatMonddE() {
+        return IgazatMonddE;
+    }
     
+
 }
